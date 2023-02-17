5abc6ff6efb1ca4182814a24cbca7d392de69a64
Anastasiia <schili@outlook.com>
2023-02-17 18:37:22 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import Throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+let formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+const STORAGE = localStorage;
 
+const handleInputData = ({ target }) => {
+  formData[target.name] = target.value;
+  STORAGE.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+if (STORAGE.getItem(STORAGE_KEY)) {
+  try {
+    formData = JSON.parse(STORAGE.getItem(STORAGE_KEY));
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+  for (let data in formData) {
+    form.elements[data].value = formData[data];
+  }
+}
+const handleFormSubmit = evt => {
+  evt.preventDefault();
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+  formData = {};
+};
+
+form.addEventListener('input', Throttle(handleInputData, 500));
+form.addEventListener('submit', handleFormSubmit);
